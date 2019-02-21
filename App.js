e287e81393186165b48fd4274462dfe1a26b433c
Max Gough <46005776+maxgoughdev@users.noreply.github.com>
2019-02-21 09:13:40 -0600

    Initial parent component

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// Import child components above..
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            
+        }
+    }
+}
